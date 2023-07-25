7ea5170042d5acfd87df8f3a099f707e5e7867f2
manishjha <manishrajkiit@gmail.com>
2023-07-25 13:23:13 +0530

    deployment update

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
+import os,sys
 from sensor.logger import logging
-from sensor.entity.config_entity import TrainingPipeLineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from sensor.pipeline import training_pipeline
 from sensor.pipeline.training_pipeline import TrainPipeline
+import os
+from sensor.entity.config_entity import TrainingPipeLineConfig
+from sensor.utils.main_utils import read_yaml_file
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR
 from fastapi import FastAPI
 from sensor.constant.application import APP_HOST, APP_PORT
 from starlette.responses import RedirectResponse
@@ -12,9 +16,15 @@ from fastapi.responses import Response
 from sensor.ml.model.estimator import ModelResolver,TargetValueMapping
 from sensor.utils.main_utils import load_object
 from fastapi.middleware.cors import CORSMiddleware
-import os,sys
-from sensor.utils.main_utils import read_yaml_file
-from sensor.constant.training_pipeline import SAVED_MODEL_DIR
+import os
+
+env_file_path=os.path.join(os.getcwd(),"env.yaml")
+
+def set_env_variable(env_file_path):
+
+    if os.getenv('MONGO_DB_URL',None) is None:
+        env_config = read_yaml_file(env_file_path)
+        os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
 app = FastAPI()
 origins = ["*"]
 
@@ -72,9 +82,9 @@ def main():
     
 
 if __name__=="__main__":
-    main()
+    #main()
     # set_env_variable(env_file_path)
-    #app_run(app, host=APP_HOST, port=APP_PORT)
+    app_run(app, host=APP_HOST, port=APP_PORT)
 
  
       
